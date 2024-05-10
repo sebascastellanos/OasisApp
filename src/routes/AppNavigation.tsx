8c0118921e2befac6fb262be7d52192bae0832c7
Sebastian <sebastian.castellanos2012@gmail.com>
2024-05-09 21:02:02 -0500

    Primer Commit

---
+++ src/routes/AppNavigation.tsx
@@ -0,0 +1,25 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import { Text, View } from "react-native";
+
+import LoginScreen from "../views/LoginScreen";
+import SignUpScreen from "../views/SignUpScreen";
+import StartScreen from "../views/StartScreen";
+import HomeScreen from "../views/HomeScreen";
+
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Start">
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="Start" component={StartScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
